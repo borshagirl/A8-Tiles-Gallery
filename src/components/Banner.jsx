dddcc5fb26aee8a5b85996dde7649188905de2b4
borshagirl <borshasabikunbashar@gmail.com>
2026-04-30 23:46:23 +0600

    Added Banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -4,7 +4,29 @@ import Marquee from "react-fast-marquee";
 
 const Banner = () => {
     return (
-        <div>Banner</div>
+        <div className="container mx-auto space-y-5">
+            <div className="text-center py-15">
+              <h1 className="text-5xl font-semibold mb-6">
+                Discover Your Perfect Aesthetic
+              </h1>
+
+              <Link
+                href="/all-tiles"
+                className="px-6 py-3 bg-black text-white rounded-full"
+              >
+                Browse Now
+              </Link>
+            </div>
+
+            <div className="shadow-lg rounded-md py-5 px-3">
+              {/* Marquee */}
+                <Marquee pauseOnHover={true} speed={130}>
+                    <div className="text-gray-500">
+                      New Arrivals | Modern Geometric Patterns | Join Community
+                    </div>
+                </Marquee>
+            </div>
+        </div>
     );
 };
 
