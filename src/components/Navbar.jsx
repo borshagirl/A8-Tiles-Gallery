facb9113e1f6bd960e2eb0b038ec8a157af67ecc
borshagirl <borshasabikunbashar@gmail.com>
2026-05-02 17:33:29 +0600

    conditionally added signOut and Login Btn and profile Avater

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,15 @@
+"use client"
+import { authClient } from "@/lib/auth-client";
+import Image from "next/image";
 import Link from "next/link";
 
 
 const Navbar = () => {
+  const { data: session, isPending } = authClient.useSession()
+  const user = session?.user
+
+  console.log(user, "session")
+
     return (
         <div className="my-5">
             <div className="flex justify-between items-center px-6 py-4 bg-white shadow-md">
@@ -13,15 +21,21 @@ const Navbar = () => {
                 <div className="flex gap-4 text-gray-600">
                   <Link href="/">Home</Link>
                   <Link href="/all-tiles">All Tiles</Link>
+                  {user && <Link href="/my-profile">Profile</Link>}
                 </div>
 
-                <div> 
-                    <Link
-                      href="/login"
-                      className="px-4 py-2 bg-black text-white rounded-lg"
-                    >
-                      Login
-                    </Link>
+                <div className="flex items-center gap-4">
+                    {isPending ? ( 
+                      "Loading..."
+                     ) : user ? (
+                      <>
+                      <img src={user?.image} alt="UserAvatar" className=" w-8 h-8 rounded-full" />
+
+                      <button onClick={async () => await authClient.signOut()}>Logout</button>
+                      </>
+                    ) : (
+                      <Link href="/login">Login</Link>
+                    )}
                 </div>
             </div>
         </div>
